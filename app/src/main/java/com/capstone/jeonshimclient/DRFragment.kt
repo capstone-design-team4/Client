de7e27111ebc642f0ad24e470d508ceac01dfc4f
BoyCho <iijokv@naver.com>
2022-05-07 02:51:53 +0900

    DRFragment, Dialog 수정

--- app/src/main/java/com/capstone/jeonshimclient/DRFragment.kt
+++ app/src/main/java/com/capstone/jeonshimclient/DRFragment.kt
@@ -1,19 +1,14 @@
 package com.capstone.jeonshimclient
 
-import android.app.Activity
-import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import com.capstone.jeonshimclient.databinding.FragmentDrBinding
 import kotlinx.android.synthetic.main.fragment_dr.*
 
 class DRFragment : Fragment() {
-    private lateinit var binding: FragmentDrBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,12 +20,14 @@ class DRFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val dialog = SettingDialog(requireContext())
-        button_DR설정.setOnClickListener {
-            dialog.setDig()
+        button_set_DR.setOnClickListener {
+            dialog.setDig(requireContext())
         }
         dialog.setOnClickedListener(object : SettingDialog.ButtonClickListener{
-            override fun onClicked(drname: String){
-                print_dr_name.text = Editable.Factory.getInstance().newEditable(drname)
+            override fun onClicked(drname: String, reductions: String, time1: String, time2: String){
+                print_dr_name.text = Editable.Factory.getInstance().newEditable("$drname")
+                print_reductions.text = Editable.Factory.getInstance().newEditable("${reductions}kWh")
+                print_time.text = Editable.Factory.getInstance().newEditable("$time1 ~ $time2")
             }
         })
     }
--- app/src/main/java/com/capstone/jeonshimclient/SettingDialog.kt
+++ app/src/main/java/com/capstone/jeonshimclient/SettingDialog.kt
@@ -1,15 +1,18 @@
 package com.capstone.jeonshimclient
 
 import android.app.Dialog
+import android.app.TimePickerDialog
 import android.content.Context
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
+import java.text.SimpleDateFormat
+import java.util.*
 
 open class SettingDialog(context: Context) {
     private val dialog = Dialog(context)
 
-    fun setDig(){
+    fun setDig(context: Context){
         dialog.show()
 
         dialog.setContentView(androidx.core.R.layout.custom_dialog)
@@ -22,12 +25,27 @@ open class SettingDialog(context: Context) {
 
         dialog.setContentView(R.layout.dialog_setting)
 
-        val edit = dialog.findViewById<EditText>(R.id.input_drname)
+        val edit_dr = dialog.findViewById<EditText>(R.id.input_drname)
+
+        val edit_time1 = dialog.findViewById<Button>(R.id.input_times1)
+        val edit_time2 = dialog.findViewById<Button>(R.id.input_times2)
+
+        val edit_reductions = dialog.findViewById<EditText>(R.id.input_reductions)
+
         val btnDone = dialog.findViewById<Button>(R.id.check)
         val btnCancel = dialog.findViewById<Button>(R.id.cancle)
 
+        edit_time1.setOnClickListener {
+            getTime(edit_time1, context)
+        }
+
+        edit_time2.setOnClickListener {
+            getTime(edit_time2, context)
+        }
+
         btnDone.setOnClickListener{
-            onClickedListener.onClicked(edit.text.toString())
+            onClickedListener.onClicked(edit_dr.text.toString(), edit_reductions.text.toString(),
+                edit_time1.text.toString(), edit_time2.text.toString())
             dialog.dismiss()
         }
 
@@ -36,10 +54,26 @@ open class SettingDialog(context: Context) {
         }
     }
     interface ButtonClickListener{
-        fun onClicked(drname: String)
+        fun onClicked(drname: String, reductions: String, time1: String, time2: String)
     }
     private lateinit var onClickedListener: ButtonClickListener
 
+    private fun getTime(button: Button, context: Context){
+
+        val cal = Calendar.getInstance()
+
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+            cal.set(Calendar.HOUR_OF_DAY, hour)
+            cal.set(Calendar.MINUTE, minute)
+
+            button.text = SimpleDateFormat("HH:mm").format(cal.time)
+        }
+
+        val tt = TimePickerDialog(context, android.R.style.Theme_Holo_Light_Dialog_NoActionBar, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true)
+        tt.getWindow()?.setBackgroundDrawableResource(android.R.color.transparent)
+        tt.show()
+
+    }
     fun setOnClickedListener(listener: ButtonClickListener){
         onClickedListener = listener
     }
