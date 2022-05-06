938baed1aba5405c6ef6d1923a8391d84d71e364
yibeomseok <vv99911@gmail.com>
2022-05-06 22:18:20 +0900

    발전 전력 공급 현황

--- app/src/main/java/com/capstone/jeonshimclient/UserFragment.kt
+++ app/src/main/java/com/capstone/jeonshimclient/UserFragment.kt
@@ -1,10 +1,15 @@
 package com.capstone.jeonshimclient
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.fragment_dr.*
 
 class UserFragment : Fragment() {
 
@@ -16,4 +21,32 @@ class UserFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_user, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loadSpinner(view)
+    }
+
+    // spinner 설정 메소드
+    fun loadSpinner(view: View){
+        val items = arrayOf("전체", "발전", "일반")
+        val spinner: Spinner = view.findViewById(R.id.spinner1)
+        val adapter: ArrayAdapter<String> = ArrayAdapter(
+            view.context,
+            android.R.layout.simple_spinner_item,
+            items
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+    }
 }
