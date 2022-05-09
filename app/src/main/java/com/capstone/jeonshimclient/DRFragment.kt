5a4133a77ff0fc7352bf524e94510c237a6f9497
BoyCho <iijokv@naver.com>
2022-05-10 00:45:32 +0900

    배터리 현황 표시

--- app/src/main/java/com/capstone/jeonshimclient/DRFragment.kt
+++ app/src/main/java/com/capstone/jeonshimclient/DRFragment.kt
@@ -1,11 +1,14 @@
 package com.capstone.jeonshimclient
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.fragment_dr.*
 
 class DRFragment : Fragment() {
@@ -38,5 +41,34 @@ class DRFragment : Fragment() {
             }
         }
         )
+        barBattey.max = 100
+        barBattey.setProgress(70)
+        barBattey.thumb = ColorDrawable(Color.TRANSPARENT)
+        barBattey.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                seekBar?.progress = 70
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
+        barDRcondition.max = 100
+        barDRcondition.setProgress(100)
+        barDRcondition.thumb = ColorDrawable(Color.TRANSPARENT)
+        barDRcondition.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                seekBar?.progress = 100
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
     }
 }
